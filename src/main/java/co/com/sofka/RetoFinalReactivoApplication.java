76e864e88c7c5d0080c78a55202b968052eec5d0
danielgil <danistcruz@gmail.com>
2022-07-12 19:57:52 -0500

    first commit

---
+++ src/main/java/co/com/sofka/RetoFinalReactivoApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoFinalReactivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoFinalReactivoApplication.class, args);
+	}
+
+}
